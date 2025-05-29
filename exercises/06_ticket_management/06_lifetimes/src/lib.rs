ee24cf7080e72f88d8124546dcfa47ca25c46593
Michal Kubek <kubek@wisdomtech.sk>
2025-05-29 18:07:05 +0200

    feat: implement 05/06_lifetimes

--- exercises/06_ticket_management/06_lifetimes/src/lib.rs
+++ exercises/06_ticket_management/06_lifetimes/src/lib.rs
@@ -1,3 +1,5 @@
+use std::vec::IntoIter;
+
 use ticket_fields::{TicketDescription, TicketTitle};
 
 // TODO: Implement the `IntoIterator` trait for `&TicketStore` so that the test compiles and passes.
@@ -33,6 +35,15 @@ impl TicketStore {
 
     pub fn iter(&self) -> std::slice::Iter<Ticket> {
         self.tickets.iter()
+    }
+}
+
+impl<'a> IntoIterator for &'a TicketStore {
+    type Item = &'a Ticket;
+    type IntoIter = std::slice::Iter<'a, Ticket>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
     }
 }
 
